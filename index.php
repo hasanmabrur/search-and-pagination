3634aa35838f16d488c2fcfa089a246dfac7c9f4
hasanmabrur <32894303+hasanmabrur@users.noreply.github.com>
2021-10-01 22:06:16 +0700

    Bootstrap 4

--- index.php
+++ index.php
@@ -5,40 +5,36 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- css bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">mabrur.my.id</a>
-    </div>
-  </div>
+<nav class="navbar navbar-dark bg-dark">
+  <a href="https://mabrur.my.id" class="navbar-brand text-white">Hasan Mabrur</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
 </nav>
-  
+
 <div class="container">
-  <div align="center">
-    <h3><b>Pencarian dan Pagination PHP</b></h3>
-    <h4><b>mabrur.my.id</b></h4>
-  </div>
+
   <!--Panel Form pencarian -->
-  <div class="row">
-    <div class="col-md-5">
-      <div class="panel panel-default">
-        <div class="panel-heading"><b>Pencarian</b></div>
-        <div class="panel-body">
+  <div class="row mt-5 mb-4">
+    <div class="col-md-8">
+    <h3><b>Pencarian dan Pagination PHP</b></h3>
+    </div>
+    <div class="col-md-4">
           <form class="form-inline" >
             <div class="form-group">
               <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Kata kunci.." required="" value="<?php if (isset($_GET['keyword']))  echo $_GET['keyword']; ?>">
             </div>
-            <button type="submit" class="btn btn-primary">Cari</button>
+            <button type="submit" class="btn btn-primary m-2">Cari</button>
             <a href="index.php" class="btn btn-danger">Reset</a>
           </form>
-        </div>
-      </div>
     </div>
   </div>
   <!-- Tabel data Siswa -->
@@ -94,7 +90,7 @@
         if($page == 1){ 
       ?>        
         <!-- link Previous Page disable --> 
-        <li class="disabled"><a href="#">Previous</a></li>
+        <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
       <?php
         }
         else{ 
@@ -102,11 +98,11 @@
 
           if($kolomkeyword==""){
           ?>
-            <li><a href="index.php?page=<?php echo $LinkPrev; ?>">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="index.php?page=<?php echo $LinkPrev; ?>">Previous</a></li>
        <?php     
           }else{
         ?> 
-          <li><a href="index.php?keyword=<?php echo $kolomkeyword;?>&page=<?php echo $LinkPrev;?>">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="index.php?keyword=<?php echo $kolomkeyword;?>&page=<?php echo $LinkPrev;?>">Previous</a></li>
          <?php
            } 
         }
@@ -137,16 +133,16 @@
         $endNumber = ($page < ($jumlahPage - $jumlahNumber))? $page + $jumlahNumber : $jumlahPage; 
         
         for($i = $startNumber; $i <= $endNumber; $i++){
-          $linkActive = ($page == $i)? ' class="active"' : '';
+          $linkActive = ($page == $i)? ' class="page-item active"' : '';
 
           if($kolomkeyword==""){
       ?>
-          <li<?php echo $linkActive; ?>><a href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+          <li <?php echo $linkActive; ?>><a class="page-link" href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
 
       <?php
         }else{
           ?>
-          <li<?php echo $linkActive; ?>><a href="index.php?keyword=<?php echo $kolomkeyword;?>&page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+          <li class="page-item"<?php echo $linkActive; ?>><a class="page-link" href="index.php?keyword=<?php echo $kolomkeyword;?>&page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
           <?php
         }
       }
@@ -156,18 +152,18 @@
       <?php       
        if($page == $jumlahPage){ 
       ?>
-        <li class="disabled"><a href="#">Next</a></li>
+        <li class="page-item" class="disabled"><a class="page-link" href="#">Next</a></li>
       <?php
       }
       else{
         $linkNext = ($page < $jumlahPage)? $page + 1 : $jumlahPage;
        if($kolomkeyword==""){
           ?>
-            <li><a href="index.php?page=<?php echo $linkNext; ?>">Next</a></li>
+            <li class="page-item"><a class="page-link" href="index.php?page=<?php echo $linkNext; ?>">Next</a></li>
        <?php     
           }else{
         ?> 
-           <li><a href="index.php?keyword=<?php echo $kolomkeyword;?>&page=<?php echo $linkNext; ?>">Next</a></li>
+           <li class="page-item"><a class="page-link" href="index.php?keyword=<?php echo $kolomkeyword;?>&page=<?php echo $linkNext; ?>">Next</a></li>
       <?php
         }
       }
